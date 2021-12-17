54cf39edc1dbe58899823d50bc83aa9fb1ffc913
Tony-Kara <eniolaanthony@gmail.com>
2021-12-17 16:14:39 +0600

    Initial Commit

---
+++ TableviewProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableviewProgrammatically
+//
+//  Created by mac on 12/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
